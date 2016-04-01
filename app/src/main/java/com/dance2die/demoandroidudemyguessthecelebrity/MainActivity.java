5ce6dfec35c9a457412bf078281a82a8fe82b26d
dance2die <dontbothermewithspam@gmail.com>
2016-04-01 07:17:40 -0400

    URL content can be retrieved using buffered reader.

--- app/src/main/java/com/dance2die/demoandroidudemyguessthecelebrity/MainActivity.java
+++ app/src/main/java/com/dance2die/demoandroidudemyguessthecelebrity/MainActivity.java
@@ -1,15 +1,53 @@
 package com.dance2die.demoandroidudemyguessthecelebrity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            HttpURLConnection urlConnection = null;
+            URL url;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+                BufferedReader buffer = new BufferedReader(reader);
+                String line;
+                while ((line = buffer.readLine()) != null){
+                    result += line;
+                }
+
+                Log.i("Result", result);
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +64,16 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+
+        try {
+            result = task.execute("http://www.posh24.com/celebrities").get();
+            Log.i("Contents of URL", result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
