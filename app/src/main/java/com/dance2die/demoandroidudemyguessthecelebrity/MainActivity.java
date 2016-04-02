38cba08a8892aff112dbec1d2c36fa47c22de961
dance2die <dontbothermewithspam@gmail.com>
2016-04-02 09:28:42 -0400

    Fully implemented

--- app/src/main/java/com/dance2die/demoandroidudemyguessthecelebrity/MainActivity.java
+++ app/src/main/java/com/dance2die/demoandroidudemyguessthecelebrity/MainActivity.java
@@ -1,28 +1,83 @@
 package com.dance2die.demoandroidudemyguessthecelebrity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
-    public class DownloadTask extends AsyncTask<String, Void, String> {
+    private final int CHOICE_COUNT = 4;
 
+    private ArrayList<String> celebURLs = new ArrayList<>();
+    private ArrayList<String> celebNames = new ArrayList<>();
+    private int correctCeleb = 0;
+    private ImageView imageView;
+    private int locationOfCorrectAnswer = 0;
+    private String[] answers = new String[CHOICE_COUNT];
+
+    private Button button0;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+
+    public void celebChosen(View view) {
+        if (view.getTag().equals(Integer.toString(locationOfCorrectAnswer))) {
+            Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Wrong! It was " + celebNames.get(correctCeleb), Toast.LENGTH_LONG).show();
+        }
+
+        createNewQuestion();
+    }
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap>{
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            URL url = null;
+
+            try {
+                url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... urls) {
             String result = "";
@@ -40,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
                     result += line;
                 }
 
-//                Log.i("Result", result);
                 return result;
 
             } catch (Exception e) {
@@ -67,6 +121,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        button0 = (Button) findViewById(R.id.button);
+        button1 = (Button) findViewById(R.id.button2);
+        button2 = (Button) findViewById(R.id.button3);
+        button3 = (Button) findViewById(R.id.button4);
+
         DownloadTask task = new DownloadTask();
         String result = null;
 
@@ -74,25 +135,55 @@ public class MainActivity extends AppCompatActivity {
             result = task.execute("http://www.posh24.com/celebrities").get();
             String[] splitResult = result.split("<div class=\"sidebarContainer\">");
 
-            Pattern p = Pattern.compile("src=\"(.*?)\"");
+            Pattern p = Pattern.compile("<img src=\"(.*?)\"");
             Matcher m = p.matcher(splitResult[0]);
 
             while (m.find()){
-//                System.out.println(m.group(1));
-                Log.i("src=", m.group(1));
+                celebURLs.add(m.group(1));
             }
 
             p = Pattern.compile("alt=\"(.*?)\"");
             m = p.matcher(splitResult[0]);
 
-            while (m.find()){
-//                System.out.println(m.group(1));
-                Log.i("alt=", m.group(1));
+            while (m.find()) {
+                celebNames.add(m.group(1));
             }
 
-
+            createNewQuestion();
 
 //            Log.i("Contents of URL", result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createNewQuestion() {
+        try {
+            Random random = new Random();
+            correctCeleb = random.nextInt(celebURLs.size());
+
+            ImageDownloader imageTask = new ImageDownloader();
+            Bitmap celebImage = null;
+            celebImage = imageTask.execute(celebURLs.get(correctCeleb)).get();
+            imageView.setImageBitmap(celebImage);
+
+            locationOfCorrectAnswer = random.nextInt(CHOICE_COUNT);
+            int incorrectAnswerLocation;
+            for (int i = 0; i < CHOICE_COUNT; i++) {
+                if (i == locationOfCorrectAnswer) {
+                    answers[i] = celebNames.get(correctCeleb);
+                } else {
+                    do {
+                        incorrectAnswerLocation = random.nextInt(CHOICE_COUNT);
+                    } while (incorrectAnswerLocation == correctCeleb);
+                    answers[i] = celebNames.get(incorrectAnswerLocation);
+                }
+            }
+
+            button0.setText(answers[0]);
+            button1.setText(answers[1]);
+            button2.setText(answers[2]);
+            button3.setText(answers[3]);
         } catch (Exception e) {
             e.printStackTrace();
         }
